e8f90beb50fe704a03f178d7b0a43b9722a4140a
rogolie <rogolie81@gmail.com>
2015-03-05 21:29:07 -0500

    Foundation & Cards
    This version stores the cards in arrays instead of using a database.  It
    shuffles the cards before play.  Presently it lists all the cards and
    visually displays the top card.  I have images for all the cards
    displayed.  The foundations display all cards.  The rules for the
    foundations still need tweaked.

--- app/controllers/game_play_controller.rb
+++ app/controllers/game_play_controller.rb
@@ -1,18 +1,47 @@
 class GamePlayController < ApplicationController
 
   helper_method :moveToF2
-  $cards = [1,2,3,4,5,6,7,8,9,10,11,12]
+  #$cards = [1,2,3,4,5,6,7,8,9,10,11,12]
+  $cards = []
   $found1 = []
   $found2 = []
   $found3 = []
   $found4 = []
+  $waste1 = []
+  $waste2 = []
+  $waste3 = []
+  $waste4 = []
+
+  #card class
+  class Card
+    attr_accessor :id, :value, :suit
+    def initialize (id, value, suit)
+      @id = id
+      @value = value
+      @suit = suit
+    end
+    def to_s; id.to_s; suit.to_s; end
+  end
+
+  #build & shuffle deck
+  @suits = %w{s c h d}
+  @ranks = %w{2 3 4 5 6 7 8 9 10 11 12 13 14}
+  @suits.each do |suit|
+    @ranks.size.times do |i|
+      id = suit + @ranks[i].to_s
+      @card = Card.new(id, @ranks[i], suit)
+      $cards << @card
+    end
+  end
+  $cards.shuffle!
+
 
   def index
 
   end
 
   def moveToF1
-    if ($cards.last).modulo(1) == 0
+    if ($cards.last.value.to_i).modulo(1) == 0
       $found1 << $cards.last
       $cards.pop
     end
@@ -20,7 +49,7 @@ class GamePlayController < ApplicationController
   end
 
   def moveToF2
-    if ($cards.last).modulo(2) == 0
+    if ($cards.last.value.to_i).modulo(2) == 0
       $found2 << $cards.last
       $cards.pop
     end
@@ -28,7 +57,7 @@ class GamePlayController < ApplicationController
   end
 
   def moveToF3
-    if ($cards.last).modulo(3) == 0
+    if ($cards.last.value.to_i).modulo(3) == 0
       $found3 << $cards.last
       $cards.pop
     end
@@ -36,7 +65,7 @@ class GamePlayController < ApplicationController
   end
 
   def moveToF4
-    if ($cards.last).modulo(4) == 0
+    if ($cards.last.value.to_i).modulo(4) == 0
       $found4 << $cards.last
       $cards.pop
     end
