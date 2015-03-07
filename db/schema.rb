653a0523ee58ecefa693d74d218ffef1cbdc26da
James Taylor II <jtsee2@gmail.com>
2015-03-07 16:39:16 -0500

    rake update, gem update, etc.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150305184136) do
+
+  create_table "cards", force: true do |t|
+    t.string   "card_id"
+    t.integer  "value"
+    t.string   "suit"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
