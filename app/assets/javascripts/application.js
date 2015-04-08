991b673b0bae864d31707d6688c1623beedcb21d
Andrew Pfundstein <apfundst@spsu.edu>
2015-04-08 14:03:09 -0400

    trying not to screw this up

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,45 @@
 
 $(function() {
 
+    var checkValidMoves = function(p1,p2,p3,p4){
+        var validMove = false;
+        validMove = checkPointPiles("DECK", p1,p2,p3,p4) || validMove;
+        validMove = checkPointPiles("waste1", p1,p2,p3,p4) || validMove;
+        validMove = checkPointPiles("waste2", p1,p2,p3,p4) || validMove;
+        validMove = checkPointPiles("waste3", p1,p2,p3,p4) || validMove;
+        validMove = checkPointPiles("waste4", p1,p2,p3,p4) || validMove;
+
+        //if cards are in deck, there is still a valid move
+        var deck = $("#DECK > img").attr('alt');
+        if(deck != null){
+            return true;
+        }
+        return validMove;
+    };
+
+    var checkPointPiles = function(pile1, p1,p2,p3,p4){
+        //check if pile is waste
+        var pile  = $("#"+pile1+" > ul > li:last-child > img").attr('alt');
+
+        //check if pile is deck
+        if(pile == null){
+            pile = $("#"+pile1+" > img").attr('alt');
+        }
+        var validMove = false;
+        if(pile != null){
+            pile = parseInt(pile.slice(1));
+            validMove = validMove || check(pile, $("#point1"), p1, 1);
+            validMove = validMove || check(pile, $("#point2"), p2, 2);
+            validMove = validMove || check(pile, $("#point3"), p3, 3);
+            validMove = validMove || check(pile, $("#point4"), p4, 4);
+        }
+        return validMove;
+    };
+
+
+    $('.waste > ul > li:last-child img').addClass('card');
+
+
     var check = function(ident, pile, pileVal, iter){
         if ((pileVal + iter) > 13)
             pileVal-=13;
@@ -32,21 +71,25 @@ $(function() {
         }
     }
 
-    $(".DECK > img").ready(function(){
-        var id  = $("#DECK > img").attr('alt');
-        id = id.slice(1);
-
-        var p1 = $("#point1 > img").attr('alt').slice(1);
-        var p2 = $("#point2 > img").attr('alt').slice(1);
-        var p3 = $("#point3 > img").attr('alt').slice(1);
-        var p4 = $("#point4 > img").attr('alt').slice(1);
-
-        check(parseInt(id), $("#point1"), parseInt(p1), 1);
-        check(parseInt(id), $("#point2"), parseInt(p2), 2);
-        check(parseInt(id), $("#point3"), parseInt(p3), 3);
-        check(parseInt(id), $("#point4"), parseInt(p4), 4);
-
-    });
+    //$(".card").click(function(){
+    //    var id  = $(this).attr('alt');
+    //    id = id.slice(1);
+    //
+    //    var p1 = $("#point1 > img").attr('alt').slice(1);
+    //    var p2 = $("#point2 > img").attr('alt').slice(1);
+    //    var p3 = $("#point3 > img").attr('alt').slice(1);
+    //    var p4 = $("#point4 > img").attr('alt').slice(1);
+    //
+    //    check(parseInt(id), $("#point1"), parseInt(p1), 1);
+    //    check(parseInt(id), $("#point2"), parseInt(p2), 2);
+    //    check(parseInt(id), $("#point3"), parseInt(p3), 3);
+    //    check(parseInt(id), $("#point4"), parseInt(p4), 4);
+    //
+    //
+    //
+    //
+    //
+    //});
 
 
 
@@ -61,6 +104,28 @@ $(function() {
         snapMode: "inner",
         snapTolerance: 30,
         stack: ".card",
+        drag: function(){
+
+            if($(this).closest('div').hasClass("waste")){
+                $( ".waste > ul > li:last-child").droppable("disable");
+                $(".waste").droppable("disable");
+            }else{
+
+                $( ".waste > ul > li:last-child").droppable("enable");
+            }
+            var id  = $(this).attr('alt');
+            id = id.slice(1);
+
+            var p1 = $("#point1 > img").attr('alt').slice(1);
+            var p2 = $("#point2 > img").attr('alt').slice(1);
+            var p3 = $("#point3 > img").attr('alt').slice(1);
+            var p4 = $("#point4 > img").attr('alt').slice(1);
+
+            check(parseInt(id), $("#point1"), parseInt(p1), 1);
+            check(parseInt(id), $("#point2"), parseInt(p2), 2);
+            check(parseInt(id), $("#point3"), parseInt(p3), 3);
+            check(parseInt(id), $("#point4"), parseInt(p4), 4);
+        },
         start: function( event, ui ) {
             $(this).data("sourceId", $(this).parent('div').attr('id') );
 
@@ -103,12 +168,12 @@ $(function() {
         snapTolerance: 30,
         stack: ".card",
         start: function (event, ui) {
-            $(this).data("sourceId", $(this).parent('div').attr('id'));
+            $(this).data("sourceId", $(this).closest('div').attr('id'));
 
 
         }
     });
-    $(".waste").droppable({
+    $("#waste1, #waste2, #waste3, #waste4").droppable({
         hoverClass: "ui-state-hover",
         drop: function( event, ui ) {
 
@@ -137,9 +202,7 @@ $(function() {
 
 
     });
-    if( $(".waste > ul").children().length < 1){
-        $(".waste").droppable({disabled: true });
-    }
+
     $( ".waste > ul > li:last-child").droppable({
         hoverClass: "ui-state-hover",
         drop: function( event, ui ) {
@@ -170,6 +233,60 @@ $(function() {
 
     });
 
+
+    $(window).load(function(){
+
+        $("#point1").droppable("disable");
+        $("#point2").droppable("disable");
+        $("#point3").droppable("disable");
+        $("#point4").droppable("disable");
+
+        if( $("#waste1 > ul").children("li").length >= 1) {
+            $("#waste1").droppable("disable");
+
+        }else{
+            $("#waste1").droppable("enable");
+        }
+        if( $("#waste2 > ul").children("li").length >= 1){
+            $("#waste2").droppable("disable");
+        }else{
+            $("#waste1").droppable("enable");
+        }
+        if( $("#waste3 > ul").children("li").length >= 1){
+            $("#waste3").droppable("disable");
+        }else{
+            $("#waste1").droppable("enable");
+        }
+        if( $("#waste4 > ul").children("li").length >= 1){
+            $("#waste4").droppable("disable");
+        }else{
+            $("#waste1").droppable("enable");
+        }
+
+        var p1 = parseInt($("#point1 > img").attr('alt').slice(1));
+        var p2 = parseInt($("#point2 > img").attr('alt').slice(1));
+        var p3 = parseInt($("#point3 > img").attr('alt').slice(1));
+        var p4 = parseInt($("#point4 > img").attr('alt').slice(1));
+
+       setTimeout(function(){
+
+           if(!checkValidMoves(p1,p2,p3,p4)){
+               //check if win
+               if(p1 + p2 + p3 + p4 == 13*4){
+                   alert("YOU WIN THE GAME! \n press New Game to play again!");
+               }
+               else{
+                   //if the deck is empty, check if lose
+                   alert("GAME OVER \nNO VALID MOVES LEFT\n press New Game to play again!");
+               }
+           }
+
+       },0);
+
+
+
+
+    });
 
     //$( "#draggable2" ).draggable({ revert: "invalid" });
     //$( "#droppable2" ).droppable({
